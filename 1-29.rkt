5a99553e5751b5f669e9cf3df478a3d957fcbc1e
Rolf Christian Jørgensen <27211248+rcjorgensen@users.noreply.github.com>
2025-02-18 07:53:10 +0100

    Add solution to exercise 1-29

---
+++ 1-29.rkt
@@ -0,0 +1,23 @@
+#lang sicp
+
+(define (cube x) (* x x x))
+
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+; assumes n is even
+(define (simpson f a b n)
+  (define h (/ (- b a) n))
+  (define (term k)
+    (* (cond
+         ((or (= k 0) (= k n)) 1)
+         ((odd? k) 4)
+         (else 2)) (f (+ a (* k h)))))
+  (* (/ h 3)
+     (sum term 0 inc n)))
+
+(simpson cube 0 1 100)
+(simpson cube 0 1 1000)
