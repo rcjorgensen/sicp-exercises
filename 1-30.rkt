7401968879e24d2cb5187c5414d246492e735b86
Rolf Christian Jørgensen <27211248+rcjorgensen@users.noreply.github.com>
2025-02-20 08:08:03 +0100

    Add solution to exercise 1-30

---
+++ 1-30.rkt
@@ -0,0 +1,17 @@
+#lang sicp
+
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ result (term a)))))
+  (iter a 0))
+
+(define (cube x) (* x x x))
+
+(define (inc x) (+ x 1))
+
+(define (sum-cubes a b)
+  (sum cube a inc b))
+
+(sum-cubes 1 10)
