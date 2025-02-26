2210d65cbc6b4ab4917a6095b353c725c68aac40
Rolf Christian Jørgensen <27211248+rcjorgensen@users.noreply.github.com>
2025-02-26 08:22:30 +0100

    Add solution to part b of exercise 1-37

--- 1-37.rkt
+++ 1-37.rkt
@@ -1,22 +1,32 @@
 #lang sicp
 
 (define (cont-frac n d k)
-  (define (cont-frac-helper n d i k)
+  (define (recurse i)
     (if (> i k)
         0
-        (/ (n i) (+ (d i) (cont-frac-helper n d (inc i) k)))))
-  (cont-frac-helper n d 1 k))
+        (/ (n i) (+ (d i) (recurse (inc i))))))
+  (recurse 1))
+
+(define (cont-frac-iter n d k)
+  (define (iter k result)
+    (if (< k 1)
+        result
+        (iter (dec k) (/ (n k) (+ (d k) result)))))
+  (iter k 0))
 
 (define (n _) 1.0)
 (define (d _) 1.0)
 
 (cont-frac n d 1)
+(cont-frac-iter n d 1)
 ; 1.0
 
 (cont-frac n d 2)
+(cont-frac-iter n d 2)
 ; 0.5
 
 (cont-frac n d 3)
+(cont-frac-iter n d 3)
 ; 0.666...
 
 (define tolerance 0.00001)
@@ -30,7 +40,18 @@
       k
       (try (inc k))))
 
+(define (try-iter k)
+  (if (close-enough? (cont-frac-iter n d k) (/ 1 phi))
+      k
+      (try-iter (inc k))))
+
 (try 1)
+(try-iter 1)
 
 (/ 1 phi)
 (cont-frac n d (try 1))
+
+(newline)
+
+(/ 1 phi)
+(cont-frac-iter n d (try 1))
