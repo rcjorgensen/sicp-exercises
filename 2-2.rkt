68390a911833b8bae7c92ef758d1c961396dafae
Rolf Christian Jørgensen <27211248+rcjorgensen@users.noreply.github.com>
2025-03-10 08:08:03 +0100

    Add solution to Exercise 2.2

---
+++ 2-2.rkt
@@ -0,0 +1,44 @@
+#lang sicp
+
+(define (make-point x y)
+  (cons x y))
+
+(define (x-point p)
+  (car p))
+
+(define (y-point p)
+  (cdr p))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+(define (make-segment s e)
+  (cons s e))
+
+(define (start-segment s)
+  (car s))
+
+(define (end-segment s)
+  (cdr s))
+
+(define (midpoint-segment s)
+  (let ((start (start-segment s))
+        (end (end-segment s)))
+    (make-point (/ (+ (x-point start) (x-point end)) 2)
+                (/ (+ (y-point start) (y-point end)) 2))))
+
+(print-point (midpoint-segment (make-segment (make-point 1 1)
+                                             (make-point 3 3))))
+; (2,2)
+
+
+(print-point (midpoint-segment (make-segment (make-point 1.0 1.0)
+                                             (make-point 3.0 2.0))))
+; (2.0,1.5)
+
+(newline)
