492967fd828e76c0fb56ded7a137c7126b0d02ec
Rolf Christian Jørgensen <27211248+rcjorgensen@users.noreply.github.com>
2025-03-21 08:13:21 +0100

    Add solution to Exercise 2.3

--- ch2.rkt
+++ ch2.rkt
@@ -41,4 +41,38 @@
                                              (make-point 3.0 2.0))))
 ; (2.0,1.5)
 
+; Exercise 2.3
 (newline)
+
+(define (square x) (* x x))
+
+(define (make-rectangle s h)
+  (cons s h))
+
+(define (height r)
+  (cdr r))
+
+(define (width r)
+  (let ((s (car r)))
+    (let ((start (start-segment s))
+          (end (end-segment s)))
+      (sqrt (+ (square (- (x-point start)
+                          (x-point end)))
+               (square (- (y-point start)
+                          (y-point end))))))))
+
+(define (perimiter r)
+  (* 2 (+ (width r) (height r))))
+
+(define (area r)
+  (* (width r) (height r)))
+
+(define rect (make-rectangle (make-segment (make-point 1.0 5.0)
+                                           (make-point 9.0 2.0))
+                             4.0))
+
+(perimiter rect)
+; 25.08
+
+(area rect)
+; 34.14
