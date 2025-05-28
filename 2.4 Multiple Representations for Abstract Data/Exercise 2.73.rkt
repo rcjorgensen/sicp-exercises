7c52ce3e2aa7f339f957e0cc2053798ba112c944
Rolf Christian Jørgensen <27211248+rcjorgensen@users.noreply.github.com>
2025-05-28 08:37:24 +0200

    Start work on Exercise 2.73

---
+++ 2.4 Multiple Representations for Abstract Data/Exercise 2.73.rkt
@@ -0,0 +1,72 @@
+#lang sicp
+
+(define (make-table)
+  (let ((local-table (list '*table*)))
+    (define (lookup key-1 key-2)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (cdr record)
+                  false))
+            false)))
+    (define (insert! key-1 key-2 value)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (set-cdr! record value)
+                  (set-cdr! subtable
+                            (cons (cons key-2 value)
+                                  (cdr subtable))))
+              (set-cdr! local-table
+                        (cons (list key-1
+                                    (cons key-2 value))
+                              (cdr local-table))))))
+      'ok)
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc!) insert!)
+            (else (error "Unknown operation -- TABLE" m))))
+    dispatch))
+
+(define operation-table (make-table))
+(define get (operation-table 'lookup-proc))
+(define put (operation-table 'insert-proc!))
+
+(display "Exercise 2.73 - Differentiation with data-directed programming") (newline)
+
+(define (variable? x) (symbol? x))
+
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp) (if (same-variable? exp var) 1 0))
+        (else ((get 'deriv (operator exp)) (operands exp)
+                                           var))))
+(define (operator exp) (car exp))
+
+(define (operands exp) (cdr exp))
+
+
+(define (install-sum-package)
+  ;; internal procedures
+  (define (addend s) (car s))
+  (define (augend s) (cdr s))
+  (define (make-sum a1 a2)
+    (cond ((=number? a1 0) a2)
+          ((=number? a2 0) a1)
+          ((and (number? a1) (number? a2)) (+ a1 a2))
+          (else (cons a1 a2))))
+  (define (deriv-sum exp var)
+    (tag (make-sum (deriv (addend exp) var) (deriv (augend exp) var))))
+
+  ;; interface to the rest of the system
+  (define (tag x) (attach-tag '+ x))
+  (put 'deriv '+ deriv-sum)
+  'done)
+
+
+
---
+++ 2.4 Multiple Representations for Abstract Data/tables.rkt
@@ -0,0 +1,63 @@
+#lang sicp
+
+;; Tables
+
+(define (assoc key records)
+  (cond ((null? records) false)
+        ((equal? key (caar records)) (car records))
+        (else (assoc key (cdr records)))))
+
+(define (lookup key table)
+  (let ((record (assoc key (cdr table))))
+    (if record
+        (cdr record)
+        false)))
+
+(define mytable (list '*table* (cons 'a 1) (cons 'b 2) (cons 'c 3)))
+
+(lookup 'a mytable)
+(lookup 'b mytable)
+(lookup 'c mytable)
+(lookup 'd mytable)
+
+(define (insert! key value table)
+  (let ((record (assoc key (cdr table))))
+    (if record
+        (set-cdr! record value)
+        (set-cdr! table
+                  (cons (cons key value) (cdr table))))))
+
+(define (make-table)
+  (list '*table*))
+
+(define mytable2 (make-table))
+mytable2
+
+(insert! 'foo 42 mytable2)
+mytable2
+
+;; Two-dimensional tables
+
+(define (lookup-2d key-1 key-2 table)
+  (let ((subtable (assoc key-1 (cdr table))))
+    (if subtable
+        (let ((record (assoc key-2 (cdr subtable))))
+          (if record
+              (cdr record)
+              false))
+        false)))
+
+(define (insert-2d! key-1 key-2 value table)
+  (let ((subtable (assoc key-1 (cdr table))))
+    (if subtable
+        (let ((record (assoc key-2 (cdr subtable))))
+          (if record
+              (set-cdr! record value)
+              (set-cdr! subtable
+                        (cons (cons key-2 value)
+                              (cdr subtable)))))
+        (set-cdr! table
+                  (cons (list key-1
+                              (cons key-2 value))
+                        (cdr table)))))
+  'ok)
