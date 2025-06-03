86c540afeab7e238b962d9b1310840eac974272b
Rolf Christian Jørgensen <27211248+rcjorgensen@users.noreply.github.com>
2025-06-03 08:16:22 +0200

    Add solutions

--- 2.4 Multiple Representations for Abstract Data/Exercise 2.73.rkt
+++ 2.4 Multiple Representations for Abstract Data/Exercise 2.73.rkt
@@ -1,7 +1,10 @@
 #lang sicp
 
+#| (#%require racket/trace) |#
+
 (define (make-table)
   (let ((local-table (list '*table*)))
+
     (define (lookup key-1 key-2)
       (let ((subtable (assoc key-1 (cdr local-table))))
         (if subtable
@@ -10,24 +13,31 @@
                   (cdr record)
                   false))
             false)))
+
     (define (insert! key-1 key-2 value)
+
       (let ((subtable (assoc key-1 (cdr local-table))))
+
         (if subtable
+
             (let ((record (assoc key-2 (cdr subtable))))
               (if record
                   (set-cdr! record value)
                   (set-cdr! subtable
                             (cons (cons key-2 value)
-                                  (cdr subtable))))
-              (set-cdr! local-table
-                        (cons (list key-1
-                                    (cons key-2 value))
-                              (cdr local-table))))))
+                                  (cdr subtable)))))
+
+            (set-cdr! local-table
+                      (cons (list key-1
+                                  (cons key-2 value))
+                            (cdr local-table)))))
       'ok)
+
     (define (dispatch m)
       (cond ((eq? m 'lookup-proc) lookup)
             ((eq? m 'insert-proc!) insert!)
             (else (error "Unknown operation -- TABLE" m))))
+
     dispatch))
 
 (define operation-table (make-table))
@@ -36,37 +46,62 @@
 
 (display "Exercise 2.73 - Differentiation with data-directed programming") (newline)
 
+#| (define (attach-tag type-tag contents) |#
+#|   (cons type-tag contents)) |#
+
 (define (variable? x) (symbol? x))
 
 (define (same-variable? v1 v2)
   (and (variable? v1) (variable? v2) (eq? v1 v2)))
 
+(define (=number? exp num) (and (number? exp) (= exp num)))
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list '+ a1 a2))))
+
+(define (addend s) (car s))
+
+(define (augend s) (cadr s))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+
+(define (multiplier p) (car p))
+
+(define (multiplicand p) (cadr p))
+
+(define (operator exp) (car exp))
+(define (operands exp) (cdr exp))
+
 (define (deriv exp var)
   (cond ((number? exp) 0)
         ((variable? exp) (if (same-variable? exp var) 1 0))
-        (else ((get 'deriv (operator exp)) (operands exp)
-                                           var))))
-(define (operator exp) (car exp))
+        (else ((get 'deriv (operator exp)) (operands exp) var))))
 
-(define (operands exp) (cdr exp))
+(put 'deriv '+
+     (lambda (operands var) (make-sum (deriv (addend operands) var) (deriv (augend operands) var))))
 
+(put 'deriv '*
+     (lambda (exp var) (make-sum
+                        (make-product (multiplier exp)
+                                      (deriv (multiplicand exp) var))
+                        (make-product (deriv (multiplier exp) var)
+                                      (multiplicand exp)))))
 
-(define (install-sum-package)
-  ;; internal procedures
-  (define (addend s) (car s))
-  (define (augend s) (cdr s))
-  (define (make-sum a1 a2)
-    (cond ((=number? a1 0) a2)
-          ((=number? a2 0) a1)
-          ((and (number? a1) (number? a2)) (+ a1 a2))
-          (else (cons a1 a2))))
-  (define (deriv-sum exp var)
-    (tag (make-sum (deriv (addend exp) var) (deriv (augend exp) var))))
-
-  ;; interface to the rest of the system
-  (define (tag x) (attach-tag '+ x))
-  (put 'deriv '+ deriv-sum)
-  'done)
-
-
-
+#| (trace deriv) |#
+(deriv '(+ x 3) 'x)
+(deriv '(* x y) 'x)
+(deriv '(* (* x y) (+ x 3)) 'x)
+#| (deriv '(** x n) 'x) |#
+#| (deriv '(** x 0) 'x) |#
+#| (deriv '(** x 1) 'x) |#
+#| (deriv '(** x 2) 'x) |#
+#| (deriv '(** x 3) 'x) |#
+#| (deriv '(** (* x y) 3) 'x) |#
